49aa932ea2d7b980f6772e8d384c7e4fe373a100
Rozalina Volchenko <volchenkoro1980@outlook.com>
2024-06-25 06:26:06 +0300

    Committing file auth/api/src/main/java/com/tencent/supersonic/auth/api/authentication/service/UserService.java

---
+++ auth/api/src/main/java/com/tencent/supersonic/auth/api/authentication/service/UserService.java
@@ -0,0 +1,31 @@
+package com.tencent.supersonic.auth.api.authentication.service;
+
+import com.tencent.supersonic.auth.api.authentication.pojo.Organization;
+import com.tencent.supersonic.auth.api.authentication.pojo.User;
+import com.tencent.supersonic.auth.api.authentication.request.UserReq;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Set;
+
+public interface UserService {
+
+    User getCurrentUser(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse);
+
+    List<String> getUserNames();
+
+    List<User> getUserList();
+
+    void register(UserReq userCmd);
+
+    String login(UserReq userCmd, HttpServletRequest request);
+
+    String login(UserReq userCmd, String appKey);
+
+    Set<String> getUserAllOrgId(String userName);
+
+    List<User> getUserByOrg(String key);
+
+    List<Organization> getOrganizationTree();
+}
